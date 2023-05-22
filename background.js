3cb6fb81091189d291e900f86e8c84ef37c705c0
Eric Xu <ericxu2024@berkeley.edu>
2023-05-22 11:40:41 -0700

    created working button that runs content script

---
+++ background.js
@@ -0,0 +1,28 @@
+console.log("background running")
+
+/*function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function doSomethingWithSelectedText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+        console.log("Got selected text " + selectedText);
+    }
+}
+
+document.onmouseup = doSomethingWithSelectedText;
+document.onkeyup = doSomethingWithSelectedText;*/
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['content.js']
+    });
+  });
---
+++ content.js
@@ -0,0 +1,30 @@
+var paragraphs = document.getElementsByTagName("p");
+
+for (p of paragraphs) {
+    p.style["background-color"] = "green";
+}
+
+
+function getSelText() {
+    var txt = '';
+    if (window.getSelection)
+    {
+        txt = window.getSelection();
+        console.log("test1");
+    }
+    else if (document.getSelection)
+    {
+        txt = document.getSelection();
+        console.log("test2");
+    }
+    else if (document.selection)
+    {
+        txt = document.selection.createRange().text;
+        console.log("test3");
+    }
+    else return;
+    console.log("test");
+}
+
+//getSelText();
+console.log("content script ran");
